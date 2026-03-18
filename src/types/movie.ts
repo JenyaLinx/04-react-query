2d7c64f1be17f5ce15fcea45e68bcac527ce68d2
Jenya Linx <sportpit2020@gmail.com>
2026-03-18 21:30:58 +0200

    third fly

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,16 +1,11 @@
-// export interface Movie {
-//   id: number;
-//   poster_path: string;
-//   backdrop_path: string;
-//   title: string;
-//   overview: string;
-//   release_date: string;
-//   vote_average: number;
-// }
-
 export interface Movie {
   id: number;
   title: string;
-  poster_path: string | null;
   overview: string;
+
+  poster_path: string | null;
+  backdrop_path: string | null;
+
+  release_date: string;
+  vote_average: number;
 }
