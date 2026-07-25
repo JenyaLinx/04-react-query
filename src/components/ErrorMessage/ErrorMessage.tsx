f0eaa1b359e10e1d8086312c8194a2c2fb01604a
Jenya Linx <sportpit2020@gmail.com>
2026-07-25 20:11:10 +0300

    Redesign error message

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -1,5 +1,19 @@
 import css from "./ErrorMessage.module.css";
 
 export default function ErrorMessage() {
-  return <p className={css.text}>There was an error, please try again...</p>;
+  return (
+    <div className={css.error}>
+      <div className={css.icon}>!</div>
+
+      <div>
+        <h3>Something went wrong</h3>
+
+        <p>
+          We couldn't load the movies.
+          <br />
+          Please try again in a moment.
+        </p>
+      </div>
+    </div>
+  );
 }
